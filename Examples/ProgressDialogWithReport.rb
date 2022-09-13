285f1a580bd5172a9e00cb659bb75ee2513c6c32
Steven Joseph Luke <steven.luke@nuix.com>
2022-09-13 12:42:10 -0400

    Updated example hiding unwanted controls

--- Examples/ProgressDialogWithReport.rb
+++ Examples/ProgressDialogWithReport.rb
@@ -16,17 +16,28 @@ rdm.addSection "Size Information", section2
 ProgressDialog.forBlock do | pd |
 	pd.set_title "Progress Report..."
 	pd.set_abort_button_visible false
+  pd.set_main_progress_visible false
 	pd.set_sub_progress_visible false
+  pd.set_log_visible false
 
-    pd.set_main_status_and_log_it "Starting to Count"
+  pd.set_main_status_and_log_it "Starting to Count"
 	pd.add_report rdm
 
 	for counter in 1..30 do
+    # Update the values in the report as needed...
 	  rdm.updateData "Summary Data", "Count", counter
+
+
+    # Also update progress when required.
+    if 15 == counter
+      pd.set_sub_status_and_log_it "Half way there."
+    end
 	  pd.set_main_progress counter, 30
+
 	  sleep 1
 	end
 
 	pd.set_main_status_and_log_it "Finished"
+  pd.set_sub_status ""
 
 end
