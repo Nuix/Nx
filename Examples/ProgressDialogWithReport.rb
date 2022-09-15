39fa92c0957eb0448f81808135f9f82bc169c24a
Steven Joseph Luke <steven.luke@nuix.com>
2022-09-15 10:41:52 -0400

    Updated ProgressDialogWithReport example script with more use of the data model

--- Examples/ProgressDialogWithReport.rb
+++ Examples/ProgressDialogWithReport.rb
@@ -1,4 +1,3 @@
-# Bootstrap the library
 require_relative "NxBootstrap.rb"
 
 java_import "com.nuix.nx.controls.models.ReportDataModel"
@@ -8,34 +7,39 @@ java_import "com.nuix.nx.controls.models.ReportDataModel"
 # the ReportDataModel.
 
 rdm = ReportDataModel.new
-section1 = {"Average" => 0.0, "Count" => 0, "Minimum" => 0.0, "Maximum" => 0.0}
+section1 = {"Average" => 0.0, "Count" => 0, "Minimum" => 100.0, "Maximum" => 0.0}
 section2 = {"Height" => 12.3, "Width" => 3, "Fill" => false}
 rdm.addSection "Summary Data", section1
 rdm.addSection "Size Information", section2
 
 ProgressDialog.forBlock do | pd |
-	pd.set_title "Progress Report..."
-	pd.set_abort_button_visible false
-  pd.set_main_progress_visible false
-	pd.set_sub_progress_visible false
-  pd.set_log_visible false
+  pd.set_title "Progress Report..."
+  pd.set_abort_button_visible false
+  pd.set_sub_progress_visible false
 
   pd.set_main_status_and_log_it "Starting to Count"
-	pd.add_report rdm
+  pd.add_report rdm
 
-	for counter in 1..30 do
-    # Update the values in the report as needed...
-	  rdm.updateData "Summary Data", "Count", counter
+  for counter in 1..30 do
+    pd.set_main_progress counter, 30
 
+    old_max = rdm.get_data_field_value("Summary Data", "Maximum").to_i
+    old_min = rdm.get_data_field_value("Summary Data", "Minimum").to_i
+    old_avg = rdm.get_data_field_value("Summary Data", "Average").to_f
 
-    # Also update progress when required.
-    if 15 == counter
-      pd.set_sub_status_and_log_it "Half way there."
-    end
-	  pd.set_main_progress counter, 30
+    new_val = rand(100) + 1
+    pd.set_sub_status "#{new_val}"
+    new_avg = ((old_avg * (counter - 1)) + new_val) / counter
+    new_max = new_val > old_max ? new_val : old_max
+    new_min = new_val < old_min ? new_val : old_min
 
-	  sleep 1
-	end
+    rdm.update_data "Summary Data", "Count", counter
+    rdm.update_data "Summary Data", "Average", '%.2f' % new_avg
+    rdm.update_data "Summary Data", "Minimum", new_min
+    rdm.update_data "Summary Data", "Maximum", new_max
+
+    sleep 1
+  end
 
 	pd.set_main_status_and_log_it "Finished"
   pd.set_sub_status ""
