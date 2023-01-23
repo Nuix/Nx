9fca49d591af784820aeb4f21a141f556e6b58f9
Steven Joseph Luke <steven.luke@nuix.com>
2023-01-23 11:20:56 -0500

    Move animations to threaded tasks

--- Java/src/main/java/com/nuix/nx/dialogs/Toast.java
+++ Java/src/main/java/com/nuix/nx/dialogs/Toast.java
@@ -3,6 +3,8 @@ package com.nuix.nx.dialogs;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Toast {
     private int toastTimeInSeconds = 5;
@@ -14,88 +16,108 @@ public class Toast {
 
     public String getLastMessage() { return lastMessage; }
 
-    public void showToast(String message, Rectangle position) {
+    public void showToast(String message, Rectangle targetPosition) {
+        if (targetPosition.x < 0 || targetPosition.y < 0)
+            throw new RuntimeException("Target position location must be >= 0");
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        if (targetPosition.x > (screenSize.width - targetPosition.width) ||
+            targetPosition.y > (screenSize.height - targetPosition.height))
+            throw new RuntimeException("Target position must not be off screen.");
+
         this.lastMessage = message;
-        this.lastPosition = position;
+        this.lastPosition = targetPosition;
 
         JWindow toastWindow = new JWindow();
         toastWindow.setBackground(new Color(0,0,0,0));
         toastWindow.setLayout(new FlowLayout());
 
-        JTextArea toastPainter2 = new JTextArea(position.height/11, position.width/11);
-        toastPainter2.setEditable(false);
+        // Get the size of the characters in the font, using "M" as a representative character for width
+        Font displayFont = new Font(Font.SERIF, Font.PLAIN, 12);
+        FontMetrics metrics = toastWindow.getFontMetrics(displayFont);
+        int fontHeight = metrics.getHeight();
+        int charWidth = metrics.charWidth('M');
+
+        int borderSize = 10;
+        // Create a Text Area - define the number of Rows and Columns to display based on the sized of the character
+        JTextArea toastPainter2 = new JTextArea((targetPosition.height-borderSize*2)/fontHeight,
+                (targetPosition.width-borderSize*2)/charWidth);
+
+        // Other text area configurations
         toastPainter2.setText(message);
-        toastPainter2.setBackground(Color.LIGHT_GRAY);
+        toastPainter2.setFont(displayFont);
+
+        toastPainter2.setEditable(false);
+        toastPainter2.setBackground(new Color(230, 230, 230));
         toastPainter2.setForeground(Color.BLACK);
-        toastPainter2.setBorder(new EmptyBorder(10, 10, 10, 10));
+        toastPainter2.setBorder(new EmptyBorder(borderSize, borderSize, borderSize, borderSize));
         toastPainter2.setOpaque(true);
         toastPainter2.setLineWrap(true);
         toastPainter2.setWrapStyleWord(true);
 
         toastWindow.add(toastPainter2);
-        toastWindow.setLocation(position.getLocation());
-        toastWindow.setSize(position.getSize());
 
-        animateIn(toastWindow);
+        // The expected size of the text display
+        Dimension painterSize = toastPainter2.getPreferredSize();
+        // Set the x position = the desired position + desired width - expected with
+        int displayX = (targetPosition.x + targetPosition.width) - painterSize.width;
+        Point startingPosition = new Point(displayX, screenSize.height);
 
-        try {
-            Thread.sleep(toastTimeInSeconds * 1000);
-        } catch (InterruptedException e) {
-            // Don't care;
-        }
+        toastWindow.setLocation(startingPosition);
+        toastWindow.setSize(painterSize);
 
-        animateOut(toastWindow);
+
+        toastWindow.setOpacity(1.0f);
+        toastWindow.setVisible(true);
+
+        Timer animateOutTask = new Timer(20, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                toastWindow.setOpacity(toastWindow.getOpacity() - 0.04f);
+
+                if (toastWindow.getOpacity() < 0.04f) {
+                    toastWindow.setVisible(false);
+                    toastWindow.dispose();
+
+                    ((Timer)e.getSource()).stop();
+                }
+            }
+        });
+        animateOutTask.setRepeats(true);
+        animateOutTask.setInitialDelay(toastTimeInSeconds * 1000);
+
+
+        Timer animateInTask = new Timer(20, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Point currentPosition = toastWindow.getLocation();
+                Point newPosition = new Point(currentPosition.x, currentPosition.y - 5);
+                toastWindow.setLocation(newPosition);
+
+                if (newPosition.y <= targetPosition.y) {
+                    ((Timer)e.getSource()).stop();
+                    animateOutTask.start();
+                }
+            }
+        });
+        animateInTask.setRepeats(true);
+        animateInTask.setInitialDelay(0);
+        animateInTask.start();
     }
 
     public void showLastToast() {
         this.showToast(lastMessage, lastPosition);
     }
 
-    private void animateIn(JWindow window) {
-        try {
-            window.setOpacity(0.0f);
-            window.setVisible(true);
-            while (window.getOpacity() < 1.0) {
-                window.setOpacity(window.getOpacity() + 0.2f);
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    // Don't care
-                }
-            }
-        } finally {
-            window.setOpacity(1.0f);
-        }
-    }
-
-    private void animateOut(JWindow window) {
-        try {
-            while(window.getOpacity() > 0.2) {
-                window.setOpacity(window.getOpacity() - 0.2f);
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    // Don't care
-                }
-            }
-        } finally {
-            window.setVisible(false);
-            window.dispose();
-        }
-    }
-
     public static void main(String[] args) {
-        System.out.println("Starting");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        System.out.println(screenSize);
         Dimension toastSize = new Dimension(500, 80);
         Point location = new Point(screenSize.width - toastSize.width, screenSize.height - toastSize.height - 40);
         Rectangle bounds = new Rectangle(location, toastSize);
-        System.out.println(bounds);
+
         new Toast().showToast("Nuix T3K Analysis\n" +
                 "Your license for the application is about to expire.  Your last day of use is " +
                 "2023-01-28.  Please contact your sales representative to extend it.",
                 bounds);
-        System.out.println("Done");
     }
 }
