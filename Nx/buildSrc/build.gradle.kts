933f220d2a6e236d38c5615cc588f05f93a43ccf
Steven Luke <97394870+sluke-nuix@users.noreply.github.com>
2023-09-11 17:54:32 -0400

    Jenkins with testing

---
+++ Nx/buildSrc/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "com.nuix.nx"
+version = "1.20.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
