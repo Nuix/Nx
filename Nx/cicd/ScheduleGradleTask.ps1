060640ce6b436fc1b3a7a5f2b89f636e593450fa
Steven Luke <97394870+sluke-nuix@users.noreply.github.com>
2023-09-18 10:29:37 -0400

    Fix array initializer in the scheduled task script

--- Nx/cicd/ScheduleGradleTask.ps1
+++ Nx/cicd/ScheduleGradleTask.ps1
@@ -12,7 +12,9 @@ $ready_state_timeout = (Get-Date).AddMinutes(5)
 
 $task_name = "nx_" + $gradleTask
 $powershell_command = "$Env:windir\System32\WindowsPowerShell\v1.0\powershell.exe"
-$powershell_argument = "-NoProfile -NonInteractive -ExecutionPolicy Bypass -File $appdir\cicd\RunGradleTask.ps1 -appdir $appdir -gradleTask $gradleTask -taskParameters $taskParameters"
+$taskParameterString = "@(" + ($taskParameters -Join ", ") + ")"
+$taskParameterString = $taskParameterString + ")
+$powershell_argument = "-NoProfile -NonInteractive -ExecutionPolicy Bypass -File $appdir\cicd\RunGradleTask.ps1 -appdir $appdir -gradleTask $gradleTask -taskParameters $taskParameterString"
 $task_action = New-ScheduledTaskAction -Execute $powershell_command `
                                        -WorkingDirectory $appdir `
                                        -Argument $powershell_argument
